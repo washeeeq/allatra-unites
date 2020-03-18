6c815f0cff70e792a0f903c56581bc7b1a534b21
wash4 <vaclav.schmuttermeier@erstegroup.com>
2020-03-18 19:26:59 +0100

    Finishing slide2.

--- app/src/main/java/ua/allatra/allatraunites/ui/activity/ui/main/SlideTwoFragment.kt
+++ app/src/main/java/ua/allatra/allatraunites/ui/activity/ui/main/SlideTwoFragment.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import kotlinx.android.synthetic.main.activity_register.*
-import kotlinx.android.synthetic.main.fragment_slide_two.*
 import ua.allatra.allatraunites.R
+import ua.allatra.allatraunites.ui.fonts.OpenSansRegularTextView
 
 /**
  * A placeholder fragment containing a simple view.
@@ -47,28 +48,53 @@ class SlideTwoFragment : Fragment() {
         pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
             setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
         }
-
-        setTxtRealitiesColors()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_slide_two, container, false)
+        val root = inflater.inflate(R.layout.fragment_slide_two, container, false)
+        val textViewRealities1 = root.findViewById<OpenSansRegularTextView>(R.id.txtRealities1)
+        val textViewPeopleHaveChance = root.findViewById<OpenSansRegularTextView>(R.id.txtPeopleHaveChance)
+        setTxtRealitiesColors(textViewRealities1)
+        setTxtPeopleHaveChance(textViewPeopleHaveChance)
+
+        return root
     }
 
-    private fun setTxtRealitiesColors(){
+    private fun setTxtRealitiesColors(textView: OpenSansRegularTextView?){
         val text1 = resources.getText(R.string.text_realities_of_our_times_1)
         val text2 = resources.getText(R.string.text_realities_of_our_times_2)
 
-        val firstPart = "$text1 $text2"
+        val wholeText = "$text1 \n\n$text2"
 
-        val spannableString = SpannableString(firstPart)
+        val spannableString = SpannableString(wholeText)
         val redColor = ForegroundColorSpan(Color.RED)
 
-        spannableString.setSpan(redColor, text1.count(),firstPart.count(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(redColor, text1.count(),wholeText.count(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        setSpannableString(textView, spannableString)
+    }
 
-        txtRealities1?.text = spannableString
+    private fun setTxtPeopleHaveChance(textView: OpenSansRegularTextView?){
+        val textPart1 = resources.getText(R.string.text_people_have_chance_1)
+        val textPart2 = resources.getText(R.string.text_people_have_chance_2)
+        val textPart3 = resources.getText(R.string.text_people_have_chance_3)
+
+        val wholeText = "$textPart1\n\n$textPart2\n\n$textPart3"
+
+        val spannableString = SpannableString(wholeText)
+        val redColor = ForegroundColorSpan(Color.RED)
+
+        spannableString.setSpan(redColor, 0, textPart1.count(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        setSpannableString(textView, spannableString)
+    }
+
+    private fun setSpannableString(textView: OpenSansRegularTextView?, spannableString: SpannableString){
+        textView?.let {
+            it.text = spannableString
+        }?: kotlin.run {
+            Log.e("setTxtRealitiesColors", "View[txtRealities1] cannot be set cause object is null.")
+        }
     }
 }
