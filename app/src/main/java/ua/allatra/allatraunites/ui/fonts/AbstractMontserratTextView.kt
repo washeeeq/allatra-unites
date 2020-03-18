de95aa9b1ae1af99e63ee12819d9823d647bdd0a
wash4 <vaclav.schmuttermeier@erstegroup.com>
2020-03-18 23:08:43 +0100

    Added 3rd slide and webview player.

--- app/src/main/java/ua/allatra/allatraunites/ui/fonts/AbstractMontserratTextView.kt
+++ app/src/main/java/ua/allatra/allatraunites/ui/fonts/AbstractMontserratTextView.kt
@@ -15,15 +15,15 @@ abstract class AbstractMontserratTextView: AppCompatTextView {
 
     private var mTypeface: Typeface? = null
 
-    constructor(context: Context?) : super(context){
+    constructor(context: Context) : super(context){
         setTypeFace()
     }
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs){
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){
         setTypeFace()
     }
 
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
