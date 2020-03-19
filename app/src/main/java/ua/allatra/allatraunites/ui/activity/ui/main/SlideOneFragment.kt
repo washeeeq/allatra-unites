aa1657495c91969fdcb4fb0193885c82b6ad4433
Roman Vanek <rvanek4@gmail.com>
2020-03-19 01:37:24 +0100

    added title image and videos

--- app/src/main/java/ua/allatra/allatraunites/ui/activity/ui/main/SlideOneFragment.kt
+++ app/src/main/java/ua/allatra/allatraunites/ui/activity/ui/main/SlideOneFragment.kt
@@ -1,12 +1,16 @@
 package ua.allatra.allatraunites.ui.activity.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import ua.allatra.allatraunites.R
+import java.util.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -27,11 +31,65 @@ class SlideOneFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_slide_one, container, false)
+
+        val mWebView1 = root.findViewById<WebView>(R.id.webViewTopLeft)
+        val mWebView2 = root.findViewById<WebView>(R.id.webViewTopRight)
+        val mWebView3 = root.findViewById<WebView>(R.id.webViewBottomLeft)
+        val mWebView4 = root.findViewById<WebView>(R.id.webViewBottomRight)
+
+        setFutureIsNowVideos(mWebView1, mWebView2, mWebView3, mWebView4)
+
 //        val textView: TextView = root.findViewById(R.id.section_label)
 //        pageViewModel.text.observe(this, Observer<String> {
 //            textView.text = it
 //        })
         return root
+    }
+
+    private fun setFutureIsNowVideos(mWebView1: WebView?, mWebView2: WebView?, mWebView3: WebView?, mWebView4: WebView?){
+
+        val videoList = when (languageCode.toLowerCase(Locale.ROOT)){
+            "en" -> FUTURE_IS_NOW_VIDEOS_EN
+            "ru" -> FUTURE_IS_NOW_VIDEOS_RU
+            "cs" -> FUTURE_IS_NOW_VIDEOS_CS
+            else -> {
+                Log.e("setFutureIsNowVideos", "Language unmapped: $languageCode, defaulting to EN.")
+                FUTURE_IS_NOW_VIDEOS_EN
+            }
+        }
+
+        mWebView1?.let {
+            val uri = "$YOUTUBE_BASE_URL${videoList[0]}"
+
+            it.settings.javaScriptEnabled = true
+            //mWebView.getSettings().setPluginState(PluginState.ON)
+            it.webChromeClient = WebChromeClient()
+            it.loadUrl(uri)
+        }
+        mWebView2?.let {
+            val uri = "$YOUTUBE_BASE_URL${videoList[1]}"
+
+            it.settings.javaScriptEnabled = true
+            //mWebView.getSettings().setPluginState(PluginState.ON)
+            it.webChromeClient = WebChromeClient()
+            it.loadUrl(uri)
+        }
+        mWebView3?.let {
+            val uri = "$YOUTUBE_BASE_URL${videoList[2]}"
+
+            it.settings.javaScriptEnabled = true
+            //mWebView.getSettings().setPluginState(PluginState.ON)
+            it.webChromeClient = WebChromeClient()
+            it.loadUrl(uri)
+        }
+        mWebView4?.let {
+            val uri = "$YOUTUBE_BASE_URL${videoList[3]}"
+
+            it.settings.javaScriptEnabled = true
+            //mWebView.getSettings().setPluginState(PluginState.ON)
+            it.webChromeClient = WebChromeClient()
+            it.loadUrl(uri)
+        }
     }
 
     companion object {
@@ -40,6 +98,20 @@ class SlideOneFragment : Fragment() {
          * fragment.
          */
         private const val ARG_SECTION_NUMBER = "section_number"
+        private const val YOUTUBE_BASE_URL = "https://www.youtube.com/embed/"
+        private const val languageCode = "ru"
+
+        private val FUTURE_IS_NOW_VIDEOS_CS = mutableListOf(
+            "LjUP2oTUpZY", "wt1ppRC4AYI", "RUnjp6VcpDc", "iLyu9av8mCE"
+        )
+
+        private val FUTURE_IS_NOW_VIDEOS_EN = mutableListOf(
+            "qyWNAg0yB4k", "_d7KEEvGpCU", "Kyf85-yi7GI", "hWHqY8Piios"
+        )
+
+        private val FUTURE_IS_NOW_VIDEOS_RU = mutableListOf(
+            "_ibC-1KK2eY", "Xv7Ns-Wt33M", "PaJuT4uLhJA", "Q69gFVmrCiI"
+        )
 
         /**
          * Returns a new instance of this fragment for the given section
